ef28885509f5a89197471eb182dbfa234b893fe7
LKD70 <lkdornan70@gmail.com>
2021-06-18 21:21:56 +0100

    init

--- src/main.js
+++ src/main.js
@@ -1,69 +1,65 @@
-'use strict';
-
-const { getNFTs, getCollections } = require('./queries.js');
 const prompts = require('prompts');
-const chalk   = require('chalk');
+const chalk = require('chalk');
+const { getNFTs, getCollections } = require('./queries');
 
 const main = async () => {
-    const response = await prompts([
+  const response = await prompts([
+    {
+      type: 'select',
+      name: 'query',
+      message: 'What are we attempting to query?',
+      choices: [{
+        title: 'NFTs',
+        description: 'Returns all NFTs for the given address/addresses',
+        value: getNFTs,
+      },
+      {
+        title: 'Collections',
+        description: 'Returns all collections for the given address/addresses',
+        value: getCollections,
+        disabled: true,
+      },
+      ],
+    },
+    {
+      type: 'select',
+      name: 'output',
+      message: 'What format would you like the output to be in?',
+      choices: [
         {
-            type: 'select',
-            name: 'query',
-            message: 'What are we attempting to query?',
-            choices: [{
-                    title: 'NFTs',
-                    description: 'Returns all NFTs for the given address/addresses',
-                    value: getNFTs
-                },
-                {
-                    title: 'Collections',
-                    description: 'Returns all collections for the given address/addresses',
-                    value: getCollections,
-                    disabled: true
-                }
-            ]
+          title: 'human',
+          description: 'Output data in a human readable way to stdout',
+          value: 'human',
         },
         {
-            type: 'select',
-            name: 'output',
-            message: 'What format would you like the output to be in?',
-            choices: [
-                {
-                        title: 'human',
-                        description: 'Output data in a human readable way to stdout',
-                        value: 'human'
-                },
-                {
-                    title: 'JSON',
-                    description: 'Output data in JSON format on stdout',
-                    value: 'json'
-                }
-            ]
+          title: 'JSON',
+          description: 'Output data in JSON format on stdout',
+          value: 'json',
         },
-        {
-            type: 'text',
-            name: 'addresses',
-            message: 'Enter flow addresses (singular, space, or comma seporated)'
-        }
-    ]);
+      ],
+    },
+    {
+      type: 'text',
+      name: 'addresses',
+      message: 'Enter flow addresses (singular, space, or comma seporated)',
+    },
+  ]);
 
-    const addresses = response.addresses.match(/(0x[a-zA-Z0-9]{16})/g);
+  const addresses = response.addresses.match(/(0x[a-zA-Z0-9]{16})/g);
 
-    if (addresses === null) {
-        console.error(chalk.red('No addresses were received'));
+  if (addresses === null) {
+    console.error(chalk.red('No addresses were received'));
+  } else {
+    const NFTs = await Promise.all(addresses.map(response.query));
+    if (response.output === 'json') {
+      console.log(JSON.stringify(NFTs, null, 2));
     } else {
-        const NFTs = await Promise.all(addresses.map(response.query));
-        if (response.output === "json") {
-            console.log(JSON.stringify(NFTs, null, 2))
-        } else {
-            console.log(NFTs.map(n => `${chalk.yellow(n.address)}:\n` +
-                n.result.map((m, d, a) => `\t${chalk.cyan(m)}${(d !== 0 && (d + 1) % 5 === 0)
-                    ? ((d + 1) === a.length ? '.' : ',\n')
-                    : ', '}`
-                ).join('')
-            ).join('\n'));
-        }
+      console.log(NFTs.map((n) => `${chalk.yellow(n.address)} (${n.result.length}):\n${
+        n.result.map((m, d, a) => `\t${chalk.cyan(m)}${(d !== 0 && (d + 1) % 5 === 0)
+          ? ((d + 1) === a.length ? '.' : ',\n')
+          : ', '}`).join('')}`).join('\n'));
     }
+  }
 };
 
 main();
--- src/queries.js
+++ src/queries.js
@@ -1,12 +1,14 @@
 const fcl = require('@onflow/fcl');
-const t   = require('@onflow/types');
+const t = require('@onflow/types');
 
-const ChainMonsters_Address = '0x93615d25d14fa337';
+const ChainmonstersAddress = '0x93615d25d14fa337';
 
-fcl.config().put("accessNode.api", 'https://access-mainnet-beta.onflow.org');
+fcl.config().put('accessNode.api', 'https://access-mainnet-beta.onflow.org');
 
-const getNFTs = async address => ({ address, result: await fcl.decode(await fcl.send([
-    fcl.script `import ChainmonstersRewards from ${ChainMonsters_Address}
+const getNFTs = async (address) => ({
+  address,
+  result: await fcl.decode(await fcl.send([
+    fcl.script`import ChainmonstersRewards from ${ChainmonstersAddress}
     pub fun main(account: Address, id: UInt64): [UInt64] {
         let collectionRef = getAccount(account)
             .getCapability(/public/ChainmonstersRewardCollection)!
@@ -14,16 +16,20 @@ const getNFTs = async address => ({ address, result: await fcl.decode(await fcl.
             ?? panic("Could not get public reward collection reference")
         return collectionRef.getIDs()}`,
     fcl.args([fcl.arg(address, t.Address), fcl.arg(3558, t.UInt64)]),
-]))});
+  ])),
+});
 
-const getCollections = async address => ({ address, result: await fcl.decode(await fcl.send([
-    fcl.script `import ChainmonstersMarketplace from ${ChainMonsters_Address}
+const getCollections = async (address) => ({
+  address,
+  result: await fcl.decode(await fcl.send([
+    fcl.script`import ChainmonstersMarketplace from ${ChainmonstersAddress}
     pub fun main(account: Address): [UInt64] {
         let collectionRef = getAccount(account)
             .getCapability(/public/ChainmonstersMarketplaceCollection)
             ?? panic("Could not get public reward collection reference")
         return collectionRef.getIDs()}`,
     fcl.args([fcl.arg(address, t.Address), fcl.arg(3558, t.UInt64)]),
-]))});
+  ])),
+});
 
 module.exports = { getNFTs, getCollections };
